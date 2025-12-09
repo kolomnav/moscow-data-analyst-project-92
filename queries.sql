09983e9be1f101545387ff620e32d3955161cb24
kolomnav <kolomnav@yandex.ru>
2025-12-09 15:22:04 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,12 +1,9 @@
 --Задача 4
-
 SELECT count(customer_id) AS customers_count
 FROM customers;
-
 -- конец 4
 -- Задача 5
-
-SELECT concat(e.first_name, ' ', e.last_name) AS seller,
+SELECT concat(e.first_name,' ',e.last_name) AS seller,
        count(s.sales_person_id) AS operations,
 FLOOR (SUM (s.quantity * p.price)) AS income
 FROM sales AS s
@@ -15,6 +12,7 @@ LEFT JOIN employees e ON s.sales_person_id = e.employee_id
 GROUP BY seller
 ORDER BY income DESC
 LIMIT 10 ;
+	 
 
 --конец  задачи 5/1
  WITH avg_check AS
@@ -70,6 +68,13 @@ GROUP BY age_category
 ORDER BY age_category ;
 
 -- Конец задачи 6/1
+	  
+															  
+																   
+				   
+																		 
+						   
+ 
 
 SELECT TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
        count(DISTINCT(c.customer_id)) AS total_customers,
@@ -84,13 +89,21 @@ ORDER BY selling_month ;
  WITH tab AS
   (SELECT concat(c.first_name, ' ', c.last_name) AS customer,
           s.customer_id,
+							  
           first_value (s.sale_date) OVER (PARTITION BY s.customer_id) AS sale_date, --   первое значение s.sale_date в разрезе id продавца
  first_value (p.price) OVER (PARTITION BY s.customer_id
                              ORDER BY s.sale_date, p.price) AS first_p, -- первое значение p.price в разрезе id продавца и отсортированному по s.sale_date, p.price
+			 
+				
+								
+																						   
+ 
+	   
  first_value (concat(e.first_name, ' ', e.last_name)) OVER (PARTITION BY s.customer_id
                                                             ORDER BY s.sale_date, p.price) AS seller -- первое значение имя продавца в разрезе id продавца и отсортированному по s.sale_date, p.price
 FROM sales s
    JOIN customers c ON s.customer_id = c.customer_id
+				
    JOIN employees e ON e.employee_id = s.sales_person_id
    JOIN products p ON p.product_id = s.product_id)
 SELECT customer,
@@ -104,5 +117,3 @@ GROUP BY customer,
          seller
 ORDER BY customer_id,
          sale_date ;
-
--- конез задачи 6/3  предыдущее решение имело идентичный результат
